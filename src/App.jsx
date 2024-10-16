cf1f83a47485e6b0b694f05e04f7a549d74486be
gauravsaini9782 <gauravjetpuriya888@gmail.com>
2024-10-16 16:23:55 +0530

    vercel.json file added

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,45 @@
 import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Navbar from "./navbar";
 import HeroSection from "./HeroSection";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import OurProducts from "./OurProducts";
 import AboutUs from "./AboutUs";
 import WhyChooseUs from "./WhyChooseUs";
 import Gallery from "./Gallery";
 import Footer from "./Footer";
-
 import Ghee from "./Ghee";
-import Honey from "./Honey"
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Honey from "./Honey";
 import BS from "./BS";
 
-function App() {
-  const [count, setCount] = useState(0);
+// Import carousel styles (if needed for your project)
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
+function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <div>
-                <HeroSection />
-                <OurProducts />
-                <WhyChooseUs />
-                <Gallery />
-              </div>
-            }
-          />
-          <Route path="/about-us" element={<AboutUs />} />
-          <Route path="/bestseller" element={<BS />} />
-          <Route path="/ghee" element={<Ghee />} />
-          <Route path="/honey" element={<Honey/>} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Navbar />
+      {/* Defining Routes */}
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <HeroSection />
+              <OurProducts />
+              <WhyChooseUs />
+              <Gallery />
+            </>
+          }
+        />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/bestseller" element={<BS />} />
+        <Route path="/ghee" element={<Ghee />} />
+        <Route path="/honey" element={<Honey />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
